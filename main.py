1fce38371a7892f76db611395c7600cdf945e349
Julien Pontoire <pontoirejulien@gmail.com>
2024-09-04 16:05:16 +0200

    Modification de l'ajout des URLs
    L'ancien ajout rajoutait une liste sous forme de string ce qui empêchait son exploitation

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ domain_url = collections.defaultdict(list)
 with gzip.open('fakenews_tweets.csv.gz', 'rt') as file:
     csv_reader = csv.DictReader(file)
     for row in tqdm.tqdm(csv_reader):
-        url = row['links']
-        domain_name = ural.get_domain_name(url)
-        if domain_name != None:
-            domain_list = domain_name.split('|')
-            for domain in domain_list:
-                domain = domain.strip()
-                domain_counter[domain] += 1
-                if len(domain_url[domain]) <= 3:
-                    domain_url[domain].append(url)
-print(domain_counter.most_common(100))
+        urls = row['links']
+        url_list = urls.split('|')
+        for url in url_list:
+            domain_name = ural.get_domain_name(url)
+            if domain_name != None:
+                domain_counter[domain_name] += 1
+                if len(domain_url[domain_name]) < 3:
+                    domain_url[domain_name].append(url)
 
-result_file = open('domain_fakenews_tweets.csv', 'w')
-file_writer = csv.writer(result_file)
-for domain_dict in domain_counter.most_common():
-    file_writer.writerow([domain_dict[0], domain_dict[1], domain_url[domain_dict[0]]])
-result_file.close()
+with open('domain_fakenews_tweets_test.csv', 'w') as result_file:
+    file_writer = csv.writer(result_file)
+    file_writer.writerow(['Domain', 'Count', 'URL'])
+    for domain_dict in domain_counter.most_common():
+        tmp = ""
+        for url in domain_url[domain_dict[0]]:
+            tmp += f"{url},"
+        tmp = tmp[:-1]
+        file_writer.writerow([domain_dict[0], domain_dict[1], tmp])
+        tmp = ""
