8b6c05d73f779506ae8c71656b90791ee0bcd272
Julien Pontoire <pontoirejulien@gmail.com>
2024-09-05 12:14:24 +0200

    Update des fonctions
    Amélioration de l'ouverture des fichiers avec un double with
    reduce_by_path : enlève un nom de domaine si son url n'a pas de chemin

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import csv
 import collections
 import ural
 import tqdm
-from shorteners import SHORTENER_DOMAINS
+import urllib.parse
 
 
 def count_domains(in_file, out_file):
     "permet de compter le nombre de fois qu'apparait un nom de domaine dans le fichier csv donné en paramètre s'il est dans le bon format et renvoie \
         le résultat dans un autre fichier csv"
     domain_counter = collections.Counter()
-    domain_url = collections.defaultdict(list)
+    domain_url = collections.defaultdict(set)
 
     with gzip.open(in_file, 'rt') as file:
         csv_reader = csv.DictReader(file)
@@ -21,16 +21,16 @@ def count_domains(in_file, out_file):
                 domain_name = ural.get_domain_name(url)
                 if domain_name != None:
                     domain_counter[domain_name] += 1
-                    if len(domain_url[domain_name]) < 3:
-                        domain_url[domain_name].append(url)
+                    if len(domain_url[domain_name]) < 10:
+                        domain_url[domain_name].add(url)
 
     with open(out_file, 'w') as result_file:
         file_writer = csv.writer(result_file)
-        file_writer.writerow(['Domain', 'Count', 'URL'])
+        file_writer.writerow(['Domain', 'Count', 'URLs'])
         for domain_dict in domain_counter.most_common():
             tmp = ""
             for url in domain_url[domain_dict[0]]:
-                tmp += f"{url},"
+                tmp += f"{url} "
             tmp = tmp[:-1]
             file_writer.writerow([domain_dict[0], domain_dict[1], tmp])
             tmp = ""
@@ -38,78 +38,73 @@ def count_domains(in_file, out_file):
 
 def reduce_by_total_url_size(in_file, out_file):
     "réduit le nombre de domaines par rapport à la taille totale des URL"
-    with open(out_file, 'w') as output_file:
-        csv_writer = csv.writer(output_file)
-        csv_writer.writerow(['Domain', 'Count', 'URL'])
-    with open(in_file) as input_file:
+    with open(in_file) as input_file, open(out_file, 'w') as output_file:
         csv_reader = csv.DictReader(input_file)
+        csv_writer = csv.DictWriter(output_file, fieldnames=csv_reader.fieldnames)
+        csv_writer.writeheader()
         for row in csv_reader:
             size = len(row['URL'])
             if size < 150:
-                with open(out_file, 'a') as output_file:
-                    csv_writer = csv.writer(output_file)
-                    csv_writer.writerow([row['Domain'],row['Count'],row['URL']])
+                csv_writer.writerow(row)
 
 
 def reduce_by_url_size(in_file, out_file):
     "réduit le nombre de domaines par rapport à la taille de chaque URL"
-    with open(out_file, 'w') as output_file:
-        csv_writer = csv.writer(output_file)
-        csv_writer.writerow(['Domain', 'Count', 'URL'])
-    with open(in_file) as input_file:
+    with open(in_file) as input_file, open(out_file, 'w') as output_file:
         csv_reader = csv.DictReader(input_file)
+        csv_writer = csv.DictWriter(output_file, fieldnames=csv_reader.fieldnames)
+        csv_writer.writeheader()
         for row in csv_reader:
-            tmp = row['URL']
-            list_url = tmp.split(',')
+            tmp = row['URLs']
+            list_url = tmp.split(' ')
             verif = True
             for url in list_url:
                 if len(url) > 50:
                     verif = False
             if verif:
-                with open(out_file, 'a') as output_file:
-                    csv_writer = csv.writer(output_file)
-                    csv_writer.writerow([row['Domain'],row['Count'],row['URL']])
+                csv_writer.writerow(row)
 
 
 def remove_shorteners(in_file, out_file):
     "supprime les noms de domaine déjà connus comme des shorteners"
-    with open(out_file, 'w') as output_file:
-        csv_writer = csv.writer(output_file)
-        csv_writer.writerow(['Domain', 'Count', 'URL'])
-    with open(in_file) as input_file:
+    with open(in_file) as input_file, open(out_file, 'w') as output_file:
         csv_reader = csv.DictReader(input_file)
+        csv_writer = csv.DictWriter(output_file, fieldnames=csv_reader.fieldnames)
+        csv_writer.writeheader()
         for row in csv_reader:
-            if not ural.is_shortened_url(row['Domain']):
-                with open(out_file, 'a') as output_file:
-                    csv_writer = csv.writer(output_file)
-                    csv_writer.writerow([row['Domain'],row['Count'],row['URL']])
+            tmp = row['URLs']
+            list_url = tmp.split(' ')
+            verif = True
+            for url in list_url:
+                if ural.is_shortened_url(url):
+                    verif = False
+            if verif:
+                csv_writer.writerow(row)
 
 
 def reduce_by_path(in_file, out_file):
     "réduit le nombre de domaines en fonction de la taille du chemin de l'URL"
-    with open(out_file, 'w') as output_file:
-        csv_writer = csv.writer(output_file)
-        csv_writer.writerow(['Domain', 'Count', 'URL'])
-    with open(in_file) as input_file:
+    with open(in_file) as input_file, open(out_file, 'w') as output_file:
         csv_reader = csv.DictReader(input_file)
+        csv_writer = csv.DictWriter(output_file, fieldnames=csv_reader.fieldnames)
+        csv_writer.writeheader()
         for row in csv_reader:
-            tmp = row['URL']
-            list_url = tmp.split(',')
+            tmp = row['URLs']
+            list_url = tmp.split(' ')
             verif = True
             for url in list_url:
-                path = ural.urlpathsplit(url)
-                if len(''.join(path)) > 20:
+                path = urllib.parse.urlsplit(url).path
+                if len(path) > 20 or len(path) <= 1:
                     verif = False
             if verif:
-                with open(out_file, 'a') as output_file:
-                    csv_writer = csv.writer(output_file)
-                    csv_writer.writerow([row['Domain'],row['Count'],row['URL']])
+                csv_writer.writerow(row)
 
 
 def main():
     "boucle principale"
     # print("test")
-    reduce_by_path('reduced_file_v3.csv', 'reduced_file_v4.csv')
+    count_domains('fakenews_tweets.csv.gz', 'csv_domains_count_url.csv')
+    # reduce_by_path('csv_reduced_2.csv', 'csv_reduced_4.csv')
 
 
 if __name__ == '__main__':
